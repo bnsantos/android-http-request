edeab724cc11af4899b65f8a7483315311a072d8
bruno <bnsantos17@gmail.com>
2014-01-06 19:43:14 -0200

    Adding action bar icon changed style of action bar (white)

--- src/com/br/bnsantos/login/example/LoginActivity.java
+++ src/com/br/bnsantos/login/example/LoginActivity.java
@@ -1,9 +1,18 @@
 package com.br.bnsantos.login.example;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+    // action bar
+    private ActionBar actionBar;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +20,37 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        actionBar = getActionBar();
+        // Hide the action bar title
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(getString(R.string.action_bar_title));
+        actionBar.setIcon(R.drawable.red_balloons_logo);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Enabling Spinner dropdown navigation
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_login_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_config_request:
+                Toast.makeText(getApplicationContext(), "Config request", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_config_server:
+                Toast.makeText(getApplicationContext(), "Config server", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
