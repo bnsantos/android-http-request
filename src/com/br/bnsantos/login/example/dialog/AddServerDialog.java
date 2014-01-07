65cdb0d48efe29927a5cbe9a77698f85f972ad1d
bruno <bnsantos17@gmail.com>
2014-01-07 13:32:00 -0200

    Organize imports

--- src/com/br/bnsantos/login/example/dialog/AddServerDialog.java
+++ src/com/br/bnsantos/login/example/dialog/AddServerDialog.java
@@ -9,7 +9,6 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import com.br.bnsantos.login.example.LoginActivity;
 import com.br.bnsantos.login.example.R;
 
 /**
--- src/com/br/bnsantos/login/example/fragments/ConfigServerFragment.java
+++ src/com/br/bnsantos/login/example/fragments/ConfigServerFragment.java
@@ -1,13 +1,11 @@
 package com.br.bnsantos.login.example.fragments;
 
-import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.Toast;
 import com.br.bnsantos.login.example.LoginActivity;
 import com.br.bnsantos.login.example.R;
 import com.br.bnsantos.login.example.adapter.ServerArrayAdapter;
